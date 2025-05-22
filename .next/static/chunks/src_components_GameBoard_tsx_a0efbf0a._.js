81e82e50d9bf04764100991d50d6e1b9372293a2
Agnivanshi <86912784+AgnivanshiR@users.noreply.github.com>
2025-05-22 13:15:06 +0530

    Add .next/static/chunks/src_components_GameBoard_tsx_a0efbf0a._.js 1747899907778

---
+++ .next/static/chunks/src_components_GameBoard_tsx_a0efbf0a._.js
@@ -0,0 +1,22 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([typeof document === "object" ? document.currentScript : undefined, {
+
+"[project]/src/components/GameBoard.tsx [app-client] (ecmascript, next/dynamic entry, async loader)": ((__turbopack_context__) => {
+
+var { g: global, __dirname } = __turbopack_context__;
+{
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "static/chunks/src_b68bca04._.js",
+  {
+    "path": "static/chunks/src_styles_game_a53fa412.css",
+    "included": [
+      "[project]/src/styles/game.css [app-client] (css)"
+    ]
+  },
+  "static/chunks/src_components_GameBoard_tsx_369d2bf4._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[project]/src/components/GameBoard.tsx [app-client] (ecmascript, next/dynamic entry)");
+    });
+});
+}}),
+}]);
