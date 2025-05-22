a096347c675407b5b0afdd7b9ce76de174c6f419
Agnivanshi <86912784+AgnivanshiR@users.noreply.github.com>
2025-05-22 13:15:14 +0530

    Add .next/static/chunks/src_components_GameBoard_tsx_369d2bf4._.js 1747899916046

---
+++ .next/static/chunks/src_components_GameBoard_tsx_369d2bf4._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_b68bca04._.js",
+  "static/chunks/src_styles_game_a53fa412.css"
+],
+    source: "dynamic"
+});
